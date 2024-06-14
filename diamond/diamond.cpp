88ed91d01dc17b99a2848077a5468627b0aa4bd5
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2024-06-14 11:54:15 -0300

    Changes in indentation

--- diamond/diamond.cpp
+++ diamond/diamond.cpp
@@ -3,28 +3,28 @@
 extern "C" {
 
 void diamond(vecOf16Words* vecIn, vecOf16Words* vecOut, int size) {
-// The depth setting is required for pointer to array in the interface.
-#pragma HLS INTERFACE m_axi port = vecIn depth = 32
-#pragma HLS INTERFACE m_axi port = vecOut depth = 32
+    // The depth setting is required for pointer to array in the interface.
+    #pragma HLS INTERFACE m_axi port = vecIn depth = 32
+    #pragma HLS INTERFACE m_axi port = vecOut depth = 32
 
-    hls::stream<vecOf16Words> c0, c1, c2, c3, c4, c5;
-    assert(size % 16 == 0);
+        hls::stream<vecOf16Words> c0, c1, c2, c3, c4, c5;
+        assert(size % 16 == 0);
 
-#pragma HLS dataflow
-    load(vecIn, c0, size);
-    compute_A(c0, c1, c2, size);
-    compute_B(c1, c3, size);
-    compute_C(c2, c4, size);
-    compute_D(c3, c4, c5, size);
-    store(c5, vecOut, size);
-}
+    #pragma HLS dataflow
+        load(vecIn, c0, size);
+        compute_A(c0, c1, c2, size);
+        compute_B(c1, c3, size);
+        compute_C(c2, c4, size);
+        compute_D(c3, c4, c5, size);
+        store(c5, vecOut, size);
+    }
 }
 
 void load(vecOf16Words* in, hls::stream<vecOf16Words>& out, int size) {
 Loop_Ld:
     for (int i = 0; i < size; i++) {
-#pragma HLS performance target_ti = 32
-#pragma HLS LOOP_TRIPCOUNT max = 32
+        #pragma HLS performance target_ti = 32
+        #pragma HLS LOOP_TRIPCOUNT max = 32
         out.write(in[i]);
     }
 }
@@ -33,8 +33,8 @@ void compute_A(hls::stream<vecOf16Words>& in, hls::stream<vecOf16Words>& out1,
                hls::stream<vecOf16Words>& out2, int size) {
 Loop_A:
     for (int i = 0; i < size; i++) {
-#pragma HLS performance target_ti = 32
-#pragma HLS LOOP_TRIPCOUNT max = 32
+        #pragma HLS performance target_ti = 32
+        #pragma HLS LOOP_TRIPCOUNT max = 32
         vecOf16Words t = in.read();
         out1.write(t * 3);
         out2.write(t * 3);
@@ -45,8 +45,8 @@ void compute_B(hls::stream<vecOf16Words>& in, hls::stream<vecOf16Words>& out,
                int size) {
 Loop_B:
     for (int i = 0; i < size; i++) {
-#pragma HLS performance target_ti = 32
-#pragma HLS LOOP_TRIPCOUNT max = 32
+        #pragma HLS performance target_ti = 32
+        #pragma HLS LOOP_TRIPCOUNT max = 32
         out.write(in.read() + 25);
     }
 }
@@ -55,8 +55,8 @@ void compute_C(hls::stream<vecOf16Words>& in, hls::stream<vecOf16Words>& out,
                int size) {
 Loop_C:
     for (data_t i = 0; i < size; i++) {
-#pragma HLS performance target_ti = 32
-#pragma HLS LOOP_TRIPCOUNT max = 32
+        #pragma HLS performance target_ti = 32
+        #pragma HLS LOOP_TRIPCOUNT max = 32
         out.write(in.read() * 2);
     }
 }
@@ -64,8 +64,8 @@ void compute_D(hls::stream<vecOf16Words>& in1, hls::stream<vecOf16Words>& in2,
                hls::stream<vecOf16Words>& out, int size) {
 Loop_D:
     for (data_t i = 0; i < size; i++) {
-#pragma HLS performance target_ti = 32
-#pragma HLS LOOP_TRIPCOUNT max = 32
+        #pragma HLS performance target_ti = 32
+        #pragma HLS LOOP_TRIPCOUNT max = 32
         out.write(in1.read() + in2.read());
     }
 }
@@ -73,8 +73,8 @@ Loop_D:
 void store(hls::stream<vecOf16Words>& in, vecOf16Words* out, int size) {
 Loop_St:
     for (int i = 0; i < size; i++) {
-#pragma HLS performance target_ti = 32
-#pragma HLS LOOP_TRIPCOUNT max = 32
+        #pragma HLS performance target_ti = 32
+        #pragma HLS LOOP_TRIPCOUNT max = 32
         out[i] = in.read();
     }
 }
--- diamond/diamond_tb.cpp
+++ diamond/diamond_tb.cpp
@@ -1,20 +1,3 @@
-/*
- * Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
- * Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include "diamond.h"
 #include <fstream>
 #include <iostream>
@@ -46,12 +29,8 @@ int main() {
     // Save the results to a file
     FILE.open("result.dat");
 
-    // Executing the DUT thrice
     for (int iter = 0; iter < 3; iter++) {
-        // Execute DUT
         diamond(test.data(), outcome.data(), totalNumWords / 16);
-
-        // Display the results
         cout << "Outcome: ";
         for (int i = 0; i < totalNumWords / 16; i++) {
             for (int j = 0; j < 16; j++) {
@@ -62,7 +41,6 @@ int main() {
         FILE.close();
     }
 
-    // Compare the results file with the golden results
     retval = system("diff --brief -w result.dat result.golden.dat");
     if (retval != 0) {
         cout << "Test failed  !!!" << endl;
